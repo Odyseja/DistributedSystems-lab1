1d8719c724c0a5f5ce3a3462c25d5e1d17811663
Odyseja <odyseja16@gmail.com>
2016-03-12 15:11:02 +0100

    fix

--- DistributedSystems-c/zad2/server.c
+++ DistributedSystems-c/zad2/server.c
@@ -9,6 +9,7 @@
 #include <strings.h>
 #include <unistd.h>
 #include <byteswap.h>
+#include <signal.h>
 
 #define BUFLEN 1000000
 
@@ -64,6 +65,16 @@ int getFilenameSize(char recvline[]){
     return __bswap_32(num);
 }
 
+char* getFilename(char recvline[], int filenameSize){
+    char* buffer = malloc(sizeof(char)*filenameSize);
+    for(int i=0; i<filenameSize; i++){
+        printf("%c", recvline[i+4]);
+        buffer[i]=recvline[i+4];
+    }
+    printf("\n");
+    return buffer;
+}
+
 static void catch_function(int signo){
     printf("Caught signal, shutting down\n");
     close(cli_fd);
@@ -71,15 +82,34 @@ static void catch_function(int signo){
     exit(-1);
 }
 
+void getAndSaveFile(){
+    char recvline[BUFLEN];
+    int len=recv(cli_fd, recvline, BUFLEN, 0);
+    printf("received bytes: %d\n", len);
+    recvline[len] = 0;
+    int filenameSize = getFilenameSize(recvline);
+    printf("Filename size: %d\n", filenameSize);
+    char* filename = getFilename(recvline, filenameSize);
+
+    if(access(filename, F_OK)!=-1){
+        remove(filename);
+    }
+    FILE* file = fopen(filename, "a+");
+    while((len=recv(cli_fd, recvline, BUFLEN, 0))>0){
+        printf("Received %d\n", len);
+        fwrite(recvline, sizeof(char), len, file);
+    }
+    fclose(file);
+}
+
 int main(int argc, char **argv) {
     if(signal(SIGINT, catch_function) == SIG_ERR){
         printf("An error occurred while setting a signal handler\n");
         return EXIT_FAILURE;
     }
-	int len;
 	int cli_len;
 	struct sockaddr_in cli_addr;
-	char recvline[BUFLEN];
+
 
 	if (argc != 2) {
 		printf("usage: %s <TCP port>\n", argv[0]);
@@ -88,20 +118,11 @@ int main(int argc, char **argv) {
 
 	// create the socket (add missing arguments)
     sock_fd=initializeConnection(atoi(argv[1]));
+    cli_fd=accept(sock_fd, (struct sockaddr*)&cli_addr, &cli_len);
+    getAndSaveFile();
 
-	while (1) {
-		// accept the connection and assign descriptor to cli_fd
-		cli_fd=accept(sock_fd, (struct sockaddr*)&cli_addr, &cli_len);
-
-		// receive data to recvline buffer with the "recv" system call and assign number of received bytes to len
-		len=recv(cli_fd, recvline, BUFLEN, 0);
-		printf("received bytes: %d\n", len);
-		recvline[len] = 0;
-
-
-
-		close(cli_fd);
-	}
+    close(sock_fd);
+    close(cli_fd);
 
 	return EXIT_SUCCESS;
 }
